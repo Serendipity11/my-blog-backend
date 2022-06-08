6b42fac7ebb64d5004c845b75256c669c360340a
serendipity11 <itestthis8@gmail.com>
2022-06-08 14:17:42 -0400

    Added body-parser

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/hello", (req, res) => res.send("Hello"));
+app.use(bodyParser.json());
 
+app.get("/hello", (req, res) => res.send("Hello"));
 app.post("/hello", (req, res) => res.send("Hello"));
 
 app.listen(8000, () => console.log("Listening on port 8000"));
