a2e2e3c8eb25bac702db9d86b59b344d5d088f8e
serendipity11 <itestthis8@gmail.com>
2022-06-08 14:12:07 -0400

    Update server.js added route

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.send("Hello");
-});
+app.get("/hello", (req, res) => res.send("Hello"));
+
+app.post("/hello", (req, res) => res.send("Hello"));
 
 app.listen(8000, () => console.log("Listening on port 8000"));
