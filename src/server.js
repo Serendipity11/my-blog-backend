8d505d18d5272f40934acfeae9a90804d7b0a3bf
serendipity11 <itestthis8@gmail.com>
2022-07-06 18:15:08 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -21,6 +21,7 @@ app.post("/api/articles/:name/add-comments", (req, res) => {
   const { username, text } = req.body;
   const articleName = req.params.name;
   articlesInfo[articleName].comments.push({ username, text });
+  res.status(200).send(articlesInfo[articleName]);
 });
 
 app.listen(8000, () => console.log("Listening on port 8000"));
