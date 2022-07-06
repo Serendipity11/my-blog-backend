717ddd37bfdf3a86ec2586f705c9febe00456732
serendipity11 <itestthis8@gmail.com>
2022-07-06 18:04:38 -0400

    Update server.js and package.json

--- src/server.js
+++ src/server.js
@@ -17,8 +17,10 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/hello", (req, res) => res.send("Hello"));
-app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}`));
-app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`));
+app.post("/api/articles/:name/add-comments", (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+  articlesInfo[articleName].comments.push({ username, text });
+});
 
 app.listen(8000, () => console.log("Listening on port 8000"));
