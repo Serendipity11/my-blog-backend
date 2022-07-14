d4694b04790367e91f9b0c8de42f6b77a96e0866
serendipity11 <itestthis8@gmail.com>
2022-07-13 22:20:19 -0400

    Update server added route and installed mongoDB

--- src/server.js
+++ src/server.js
@@ -17,9 +17,14 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.get("/api/articles/:name", (req, res) => {
+  const articleName = req.params.name;
+});
+
 app.post("/api/articles/:name/add-comments", (req, res) => {
   const { username, text } = req.body;
   const articleName = req.params.name;
+
   articlesInfo[articleName].comments.push({ username, text });
   res.status(200).send(articlesInfo[articleName]);
 });
