78ef854f030903e45928f31417ec42137cc39d63
serendipity11 <itestthis8@gmail.com>
2022-07-21 11:31:22 -0400

    Update server.js refactored the code

--- src/server.js
+++ src/server.js
@@ -33,21 +33,13 @@ const withDB = async (operations, res) => {
 };
 
 app.get("/api/articles/:name", async (req, res) => {
-  try {
+  withDB(async (db) => {
     const articleName = req.params.name;
-    const client = await MongoClient.connect("mongodb://localhost:27017", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    const db = client.db("myblog");
     const articleInfo = await db
       .collection("articles")
       .findOne({ name: articleName });
     res.status(200).json(articleInfo);
-    client.close();
-  } catch (error) {
-    res.status(500).json({ message: "Error connecting to db", error });
-  }
+  });
 });
 
 app.post("/api/articles/:name/add-comments", (req, res) => {
