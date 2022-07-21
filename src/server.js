86d6928383aa2ab3b272d51a751b097ab3fdce0f
serendipity11 <itestthis8@gmail.com>
2022-07-21 11:26:52 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -18,6 +18,20 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const withDB = async (operations, res) => {
+  try {
+    const client = await MongoClient.connect("mongodb://localhost:27017", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    const db = client.db("myblog");
+    operations(db);
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "Error connecting to db", error });
+  }
+};
+
 app.get("/api/articles/:name", async (req, res) => {
   try {
     const articleName = req.params.name;
